ee17d9d6accf83a3b46b2029a2c4c81e38876799
Justin Armstrong <armstrongjustin034@gmail.com>
2024-12-30 21:37:36 -0800

    complete fizzbuzz#10

--- exercises/9-pyramid.js
+++ exercises/9-pyramid.js
@@ -14,6 +14,27 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
-
+function pyramid(n) {
+const mid = Math.floor((2*n-1)/2)
+for (let row = 0; row < n; row++) {
+    let line = ''
+    for (let col = 0; col < 2*n-1; col++) {
+        if (col >= mid - row && col <= mid + row) {
+            line += '#'
+        } else {
+            line += ' '
+        }
+    }
+    console.log(line)
+}
+}
 pyramid(3)
+
+const anotherPyramid = n => {
+    for (let i=1; i<n; i++) {
+        let hashes = '#'.repeat(2*i-1)
+        let spaces = ' '.repeat(n-i)
+        console.log(spaces+hashes+spaces)
+    }
+}
+anotherPyramid(5)
